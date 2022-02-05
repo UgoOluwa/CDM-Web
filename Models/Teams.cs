58027cc812450865a604385dd8cd5c90b3809f5c
Ugochukwu Odunukwe <ugoluwa@gmail.com>
2022-02-05 21:01:12 +0100

    initial commit

---
+++ Models/Teams.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CDM.WEB.Models
+{
+    public enum Teams
+    {
+        [Display(Name = "Protocol")]
+        Protocol = 1,
+        [Display(Name = "Prayer")]
+        Prayer,
+        [Display(Name = "WelFare")]
+        Welfare,
+        [Display(Name = "Choir")]
+        Choir,
+        [Display(Name = "Technical")]
+        Technical,
+        [Display(Name = "Organizing")]
+        Organizing
+    }
+}
